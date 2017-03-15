f5a2a7cb36119fdd942f96e07868ba812a74caff
何俊红 <704492613@qq.com>
2017-03-15 23:00:06 +0800

    收支明细

--- src/page/operation/moneyHistory/list/Index.vue
+++ src/page/operation/moneyHistory/list/Index.vue
@@ -2,75 +2,56 @@
     <div>
         <div class="container-body">
             <div class="header">
-                <div class="search">
-                    <el-form :model="form" ref="form" label-width="120px">
-                        <el-form-item label="订单编号" prop="orderNo">
-                            <el-input v-model.number="form.orderNo"></el-input>
-                        </el-form-item>
-                        <el-form-item label="交易时间">
-                            <!-- <el-col :span="8">
-                                <el-form-item prop="region1">
-                                    <el-select class="select" v-model="form.region1" @change="selectRegion1List" placeholder="全国">
-                                        <el-option :label="item.name" :value="item.code" v-for="(item,index) in region1List"></el-option>
-                                    </el-select>
-                                </el-form-item>
-                            </el-col>
-                            <el-col :span="8">
-                                <el-form-item prop="region2">
-                                    <el-select class="select" v-model="form.region2" @change="selectRegion2List" placeholder="全国">
-                                        <el-option :label="item.name" :value="item.code" v-for="(item,index) in region2List"></el-option>
-                                    </el-select>
-                                </el-form-item>
-                            </el-col> -->
+                <div>
+                    <el-form ref="form" :model="form" label-width="80px">
+                        <el-form-item label="订单编号">
+                            <el-input v-model="form.orderNo"></el-input>
                         </el-form-item>
                         <el-form-item label="交易类型">
-                            <el-col :span="8">
-                                <el-form-item prop="tradeName">
-                                    <el-select class="select" v-model="form.tradeName" placeholder="全部">
+                            <el-col :span="21">
+                                <el-form-item prop="status">
+                                    <el-select class="select" v-model="form.status" placeholder="全部">
                                         <el-option :label="item.name" :value="item.code" v-for="(item,index) in tradeList"></el-option>
                                     </el-select>
                                 </el-form-item>
                             </el-col>
                         </el-form-item>
-                        <el-form-item label="分成地区" v-if="managerType > 0">
-                            <el-col :span="8">
-                                <el-form-item prop="region1">
-                                    <el-select class="select" v-model="form.region1" @change="selectRegion1List" placeholder="全国">
-                                        <el-option :label="item.name" :value="item.code" v-for="(item,index) in region1List"></el-option>
-                                    </el-select>
-                                </el-form-item>
+                        <el-form-item label="交易时间">
+                            <el-col :span="11">
+                                <el-date-picker type="date" placeholder="选择日期" v-model="form.sTime" style="width: 100%;"></el-date-picker>
                             </el-col>
-                            <el-col :span="8">
-                                <el-form-item prop="region2">
-                                    <el-select class="select" v-model="form.region2" @change="selectRegion2List" placeholder="全国">
-                                        <el-option :label="item.name" :value="item.code" v-for="(item,index) in region2List"></el-option>
-                                    </el-select>
-                                </el-form-item>
-                            </el-col>
-                            <el-col :span="8">
-                                <el-form-item prop="region3">
-                                    <el-select class="select" v-model="form.region3" placeholder="全国">
-                                        <el-option :label="item.name" :value="item.code" v-for="(item,index) in region3List"></el-option>
-                                    </el-select>
-                                </el-form-item>
+                            <el-col class="line" :span="1">-</el-col>
+                            <el-col :span="11">
+                                <el-date-picker type="date" placeholder="选择日期" v-model="form.eTime" style="width: 100%;"></el-date-picker>
                             </el-col>
                         </el-form-item>
                         <el-form-item>
-                            <el-button type="primary" @click="submitForm('form')">提交</el-button>
-                            <el-button @click="resetForm('form')">重置</el-button>
+                            <el-button type="primary" @click="submitForm()">提交</el-button>
+                            <el-button @click="resetForm()">重置</el-button>
                         </el-form-item>
                     </el-form>
                 </div>
+
+                <div>
+                    <div class="line">
+                        <b style="font-size: 20px" class="line">平台交易金额: {{maxPrice}}元</b>
+                    </div>
+                    <div class="line" style="margin-top: 10px">
+                        <b style="font-size: 20px;">平台累计抽成金额: {{maxPercentagePrice}}元</b>
+                        <b style="font-size: 20px;padding-left: 20px">平台累计会员卡收益: {{maxVipPrice}}元</b>
+                        <b style="font-size: 20px;padding-left: 20px">代理商累计抽成金额: {{regionPercentagePrice}}元</b>
+                    </div>
+                </div>
             </div>
-            <div class="table">
-                <el-table :data="userStatisticsList" border style="width: 100%">
-                    <el-table-column prop="day_name" align="center" label="统计类型" min-width="90">
+            <div class="table" style="margin-top: 10px">
+                <el-table :data="moneyHistoryList" border style="width: 100%">
+                    <el-table-column prop="price_info" align="center" label="订单编号" min-width="90">
                     </el-table-column>
-                    <el-table-column prop="all_num" align="center" label="总注册用户数" min-width="120">
+                    <el-table-column prop="remark" align="center" label="交易类型" min-width="130">
                     </el-table-column>
-                    <el-table-column prop="register_num" align="center" label="当日注册用户数" min-width="130">
+                    <el-table-column prop="price" align="center" label="抽成金额" min-width="120">
                     </el-table-column>
-                    <el-table-column prop="login_num" align="center" label="当日活跃用户数" min-width="120">
+                    <el-table-column prop="create_time" align="center" label="结算时间" min-width="120">
                     </el-table-column>
                 </el-table>
                 <el-pagination class="el-pagination" @current-change="handleCurrentChange" :page-size="10" layout="total, prev, pager, next" :total="total">
@@ -85,52 +66,55 @@ export default {
     data() {
         return {
             form: {
-                orderNo: '',
-                region1: '',
-                region2: '',
-                region3: '',
-                tradeName: ''
+              orderNo: '',
+              status: '',
+              sTime: '',
+              eTime: ''
             },
-            region1List: [],
-            region2List: [],
-            region3List: [],
             tradeList: [],
-            userStatisticsList: [],
+            moneyHistoryList: [],
             page: 1,
             total: 0,
-            userInfo: NormalHelper.userInfo(),
-            managerType: 0
+            maxPrice: 0,
+            maxPercentagePrice: 0,
+            maxVipPrice: 0,
+            regionPercentagePrice: 0
         }
     },
     mounted() {
 
-        this.managerType = this.userInfo["manager_type"];
-        if (this.managerType <= 0) {
-            this.form.region3 = this.userInfo["region_code"];
-        } else {
-            this.getRegionList();
-        }
-        this.getUserStatistics();
+        this.getMoneyHistoryList();
+        this.getMaxPriceInfo();
         this.getTradeList();
     },
     methods: {
-        getRegionList() {
+        getTradeList() {
+            this.tradeList = [{
+                name: "全部",
+                code: 0
+            },{
+                name: "收入",
+                code: 1
+            },{
+                name: "提现",
+                code: 2
+            }];
+        },
+        getMaxPriceInfo() {
             var param = {
-                c: 'Zb',
-                m: 'Region',
-                a: 'getRegionList',
-                fixAll: 1
+                c: 'Admin',
+                m: 'MoneyHistory',
+                a: 'getMaxPriceInfo'
             };
+
             var p_obj = {
                 action: '',
                 param: param,
                 success: (response) => {
-                    this.region1List = response;
-                    this.region2List = response[0].children;
-                    this.region3List = response[0].children[0].children;
-                    this.form.region1 = response[0].code;
-                    this.form.region2 = response[0].children[0].code;
-                    this.form.region3 = response[0].children[0].children[0].code;
+                    this.maxPrice = response.maxPrice;
+                    this.maxPercentagePrice = response.maxPercentagePrice;
+                    this.maxVipPrice = response.maxVipPrice;
+                    this.regionPercentagePrice = response.regionPercentagePrice;
                 },
                 fail: (response) => {
                     NormalHelper.alert(this, response, 'error');
@@ -138,49 +122,34 @@ export default {
             };
             AjaxHelper.GetRequest(p_obj);
         },
-        getTradeList() {
-            this.tradeList = [{
-                name: "提现中",
-                code: 1
-            }];
-        },
-        selectRegion1List() {
-            for (var i = 0; i < this.region1List.length; i++) {
-                if (this.form.region1 == this.region1List[i].code) {
-                    this.region2List = this.region1List[i].children;
-                    this.region3List = this.region1List[i].children[0].children;
-                    this.form.region2 = this.region1List[i].children[0].code;
-                    this.form.region3 = this.region1List[i].children[0].children[0].code;
-                }
-            }
-        },
-        selectRegion2List() {
-            for (var i = 0; i < this.region2List.length; i++) {
-                if (this.form.region2 == this.region2List[i].code) {
-                    this.region3List = this.region2List[i].children;
-                    this.form.region3 = this.region2List[i].children[0].code;
-                }
-            }
-        },
-        getUserStatistics() {
+        getMoneyHistoryList() {
             var param = {
                 c: 'Admin',
-                m: 'Report',
-                a: 'userStatistics'
+                m: 'MoneyHistory',
+                a: 'getMoneyHistoryList',
+                page: this.page,
+                row: 10
             };
 
-            if (this.form.region3 != "" && this.form.region3 != '1') {
-                param.region = this.form.region3;
+            if(this.form.orderNo.length > 0){
+                param["orderNo"] = this.form.orderNo;
             }
+            if(this.form.status > 0){
+                param["status"] = this.form.status;
+            }
+            if(this.form.sTime > 0){
+                param["sTime"] = this.form.sTime;
+            }
+            if(this.form.eTime > 0){
+                param["eTime"] = this.form.eTime;
+            }
+
             var p_obj = {
                 action: '',
                 param: param,
                 success: (response) => {
-
-                    this.userStatisticsList = [];
-                    for (var item in response) {
-                        this.userStatisticsList.push(response[item]);
-                    }
+                    this.moneyHistoryList = response.list;
+                    this.total = parseInt(response.total);
                 },
                 fail: (response) => {
                     NormalHelper.alert(this, response, 'error');
@@ -189,28 +158,15 @@ export default {
             AjaxHelper.GetRequest(p_obj);
         },
         submitForm(formName) {
-            this.$refs[formName].validate((valid) => {
-                if (valid) {
-                    if (this.form.region3 != "") {
-                        this.page = 1;
-                        this.getUserStatistics();
-                    }
-                } else {
-                    return false;
-                }
-            });
+            this.page = 1;
+            this.getMoneyHistoryList();
         },
         resetForm(formName) {
-            this.region2List = this.region1List[0].children;
-            this.region3List = this.region1List[0].children[0].children;
             this.$refs[formName].resetFields();
-            this.form.region1 = this.region1List[0].code;
-            this.form.region2 = this.region2List[0].code;
-            this.form.region3 = this.region3List[0].code;
         },
         handleCurrentChange(val) {
             this.page = val;
-            this.getRegionManagerList();
+            this.getMoneyHistoryList();
         }
     },
     destroyed() {}
