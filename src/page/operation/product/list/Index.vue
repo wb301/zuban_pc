3eff94dbd6e48a1ceae982bbea80a3f364f4601a
LG <171736699@qq.com>
2017-03-15 23:53:37 +0800

    内容审核

--- src/page/operation/product/list/Index.vue
+++ src/page/operation/product/list/Index.vue
@@ -2,33 +2,20 @@
     <div>
         <div class="container-body">
             <div class="header">
-                <div class="add-btn">
-                    <el-button>新增代理商</el-button>
-                </div>
                 <div class="search">
                     <el-form :model="form" ref="form" label-width="120px">
-                        <el-form-item label="代理商手机号码" prop="phone">
-                            <el-input v-model.number="form.phone"></el-input>
-                        </el-form-item>
-                        <el-form-item label="代理商类型">
+                        <el-form-item label="服务类型">
                             <el-col :span="8">
-                                <el-form-item prop="region1">
-                                    <el-select class="select" v-model="form.region1" @change="selectRegion1List" placeholder="全国">
-                                        <el-option :label="item.name" :value="item.code" v-for="(item,index) in region1List"></el-option>
+                                <el-form-item prop="cate1">
+                                    <el-select class="select" v-model="form.cate1" @change="selectCate1List" placeholder="全部">
+                                        <el-option :label="item.category_name" :value="item.id" v-for="(item,index) in cate1List"></el-option>
                                     </el-select>
                                 </el-form-item>
                             </el-col>
                             <el-col :span="8">
-                                <el-form-item prop="region2">
-                                    <el-select class="select" v-model="form.region2" @change="selectRegion2List" placeholder="全国">
-                                        <el-option :label="item.name" :value="item.code" v-for="(item,index) in region2List"></el-option>
-                                    </el-select>
-                                </el-form-item>
-                            </el-col>
-                            <el-col :span="8">
-                                <el-form-item prop="region3">
-                                    <el-select class="select" v-model="form.region3" placeholder="全国">
-                                        <el-option :label="item.name" :value="item.code" v-for="(item,index) in region3List"></el-option>
+                                <el-form-item prop="cate2">
+                                    <el-select class="select" v-model="form.cate2" placeholder="全部">
+                                        <el-option :label="item.category_name" :value="item.id" v-for="(item,index) in cate2List"></el-option>
                                     </el-select>
                                 </el-form-item>
                             </el-col>
@@ -44,23 +31,22 @@
                 <el-table :data="agentList" border style="width: 100%">
                     <el-table-column prop="id" align="center" label="编号" min-width="90">
                     </el-table-column>
-                    <el-table-column prop="nick_name" align="center" label="名称" min-width="120">
+                    <el-table-column prop="image_list" align="center" label="服务图片" min-width="120">
                     </el-table-column>
-                    <el-table-column prop="account" align="center" label="手机号" min-width="130">
+                    <el-table-column prop="priceInfo" align="center" label="服务类型/价格" min-width="130">
                     </el-table-column>
-                    <el-table-column prop="manager_type_name" align="center" label="类型" min-width="120">
+                    <el-table-column prop="region_name" align="center" label="服务地区" min-width="120">
                     </el-table-column>
-                    <el-table-column prop="region_name" align="center" label="所属地区" min-width="180">
+                    <el-table-column prop="product_info" align="center" label="补充意见" min-width="180">
                     </el-table-column>
-                    <el-table-column prop="create_time" align="center" label="添加时间" min-width="185">
+                    <el-table-column prop="userInfo.nick_name" align="center" label="发布人" min-width="185">
                     </el-table-column>
-                    <el-table-column prop="status_name" align="center" label="帐号状态" min-width="100">
+                    <el-table-column prop="update_time" align="center" label="发布时间" min-width="100">
+                    </el-table-column>
+                    <el-table-column prop="status_name" align="center" label="状态" min-width="100">
                     </el-table-column>
                     <el-table-column fixed="right" align="center" label="操作" width="140">
                         <template scope="scope">
-                            <el-button @click="edit(scope.$index, agentList)" size="small">
-                                编辑
-                            </el-button>
                             <el-button @click="changeState(scope.$index, agentList)" size="small">
                                 {{scope.row.status==1?"关闭":"开启"}}
                             </el-button>
@@ -79,14 +65,11 @@ export default {
     data() {
         return {
             form: {
-                phone: '',
-                region1: '',
-                region2: '',
-                region3: ''
+                cate1: '',
+                cate2: ''
             },
-            region1List: [],
-            region2List: [],
-            region3List: [],
+            cate1List: [],
+            cate2List: [],
             agentList: [],
             total: 0,
             page: 1
@@ -94,59 +77,47 @@ export default {
         }
     },
     mounted() {
-        this.getRegionList();
-        this.getRegionManagerList();
+        this.getCateList();
+        this.getShowProductList();
     },
     methods: {
-        getRegionList() {
+        getCateList() {
             var param = {
-                c: 'Zb',
-                m: 'Region',
-                a: 'getRegionList',
+                c: 'Admin',
+                m: 'Category',
+                a: 'getCategoryList',
                 fixAll: 1
             };
             var p_obj = {
                 action: '',
                 param: param,
                 success: (response) => {
-                    this.region1List = response;
-                    this.region2List = response[0].children;
-                    this.region3List = response[0].children[0].children;
-                    this.form.region1 = response[0].code;
-                    this.form.region2 = response[0].children[0].code;
-                    this.form.region3 = response[0].children[0].children[0].code;
+                    this.cate1List = response;
+                    this.cate2List = response[0].children;
+                    this.form.cate1 = response[0].id;
+                    this.form.cate2 = response[0].children[0].id;
                 }
             };
             AjaxHelper.GetRequest(p_obj);
         },
-        selectRegion1List() {
-            for (var i = 0; i < this.region1List.length; i++) {
-                if (this.form.region1 == this.region1List[i].code) {
-                    this.region2List = this.region1List[i].children;
-                    this.region3List = this.region1List[i].children[0].children;
-                    this.form.region2 = this.region1List[i].children[0].code;
-                    this.form.region3 = this.region1List[i].children[0].children[0].code;
+        selectCate1List() {
+            for (var i = 0; i < this.cate1List.length; i++) {
+                if (this.form.cate1 == this.cate1List[i].id) {
+                    this.cate2List = this.cate1List[i].children;
+                    this.form.cate2 = this.cate1List[i].children[0].id;
                 }
             }
         },
-        selectRegion2List() {
-            for (var i = 0; i < this.region2List.length; i++) {
-                if (this.form.region2 == this.region2List[i].code) {
-                    this.region3List = this.region2List[i].children;
-                    this.form.region3 = this.region2List[i].children[0].code;
-                }
-            }
-        },
-        getRegionManagerList() {
+        getShowProductList() {
             var param = {
                 c: 'Admin',
-                m: 'User',
-                a: 'getRegionManagerList',
+                m: 'Product',
+                a: 'getShowProductList',
                 page: this.page,
                 row: 10
             };
-            if (this.form.region3 != "" && this.form.region3 != '1') {
-                param.region_code = this.form.region3;
+            if (this.form.cate2 != 0 && this.form.cate2 != 1) {
+                param.categoryId = this.form.cate2;
             }
             var p_obj = {
                 action: '',
@@ -154,7 +125,14 @@ export default {
                 success: (response) => {
                     for (var i = 0; i < response.list.length; i++) {
                         response.list[i].status_name = response.list[i].status == 1 ? "开启" : "关闭";
-                        response.list[i].manager_type_name = response.list[i].manager_type == 1 ? "平台" : "区域";
+                        var type = response.list[i].price_type;
+                        var info = "小时";
+                        if(type == 2){
+                            info = "天";
+                        }else if(type == 3){
+                            info = "次";
+                        }
+                        response.list[i].priceInfo = response.list[i].price+"元/"+info;
                     }
                     this.agentList = response.list;
                     this.total = parseInt(response.total);
@@ -162,33 +140,29 @@ export default {
             };
             AjaxHelper.GetRequest(p_obj);
         },
-        edit(index, rows) {
-
-        },
         changeState(index, rows) {
             var param = {
-                c: 'Admin',
-                m: 'User',
-                a: 'updRegionManagerStatus',
-                id: rows[index].id,
-                status: rows[index].status
-            };
-            var p_obj = {
-                action: '',
-                param: param,
-                success: (response) => {
-                    row[index].status = row[index].status == 1 ? 0 : 1;
-                    row[index].status_name = row[index].status == 1 ? "开启" : "关闭";
+                productInfo:{
+                    id: rows[index].id,
+                    status: rows[index].status
                 }
             };
-            AjaxHelper.GetRequest(p_obj);
+            var p_obj = {
+                action: '&c=Admin&m=Product&a=updateProductBySys',
+                param: param,
+                success: (response) => {
+                    rows[index].status = rows[index].status == 1 ? 0 : 1;
+                    rows[index].status_name = rows[index].status == 1 ? "开启" : "关闭";
+                }
+            };
+            AjaxHelper.PostRequest(p_obj);
         },
         submitForm(formName) {
             this.$refs[formName].validate((valid) => {
                 if (valid) {
-                    if (this.form.region3 != "") {
+                    if (this.form.cate2 != "") {
                         this.page = 1;
-                        this.getRegionManagerList();
+                        this.getShowProductList();
                     }
                 } else {
                     return false;
@@ -196,16 +170,14 @@ export default {
             });
         },
         resetForm(formName) {
-            this.region2List = this.region1List[0].children;
-            this.region3List = this.region1List[0].children[0].children;
+            this.cate2List = this.cate1List[0].children;
             this.$refs[formName].resetFields();
-            this.form.region1 = this.region1List[0].code;
-            this.form.region2 = this.region2List[0].code;
-            this.form.region3 = this.region3List[0].code;
+            this.form.cate1 = this.cate1List[0].id;
+            this.form.cate2 = this.cate2List[0].id;
         },
         handleCurrentChange(val) {
             this.page = val;
-            this.getRegionManagerList();
+            this.getShowProductList();
         }
     },
     destroyed() {}
