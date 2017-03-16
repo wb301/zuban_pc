ab97725898789f993f49f9b05da4290041dad265
何俊红 <704492613@qq.com>
2017-03-16 22:43:57 +0800

    二维码

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -3,6 +3,7 @@
         <div class="container-body">
             <h1>欢迎使用租伴网后台管理系统V1.0</h1>
         </div>
+        <div id="qrCode"></div>
     </div>
 </template>
 <script>
@@ -10,9 +11,24 @@ export default {
     name: 'hello',
     data() {
         return {
-
+            userInfo: NormalHelper.userInfo()
         }
-    }
+    },
+    mounted() {
+        this.$nextTick(function () {
+            require ('../../../lib/common/jquery.qrcode.min.js');
+            this.qrcode();
+        })
+    },
+    methods: {
+        qrcode() {
+            $("#qrCode").qrcode({
+                text: "http://www.baidu.com",
+                width:200,
+                height:200
+            })
+        }
+    },
 }
 </script>
 <style lang="less" scoped>
