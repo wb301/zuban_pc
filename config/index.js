487c8334eb945e3f76836d99a45a14635922dcea
LG <171736699@qq.com>
2017-03-16 23:01:34 +0800

    pack 配置

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
         index: path.resolve(__dirname, '../admin/index.html'),
         assetsRoot: path.resolve(__dirname, '../admin'),
         assetsSubDirectory: 'static',
-        assetsPublicPath: '/youfan/web/admin/',
+        assetsPublicPath: '/zuban_pc/console/',
         productionSourceMap: true,
         // Gzip off by default as many popular static hosts such as
         // Surge or Netlify already gzip all static assets for you.
