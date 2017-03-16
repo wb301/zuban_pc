c75d91b0556727459dcd66da0233d8efe84e8257
何俊红 <704492613@qq.com>
2017-03-16 23:12:32 +0800

    二维码

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -3,7 +3,7 @@
         <div class="container-body">
             <h1>欢迎使用租伴网后台管理系统V1.0</h1>
         </div>
-        <div id="qrCode"></div>
+        <div class="container-qrCode" v-if="isManager" id="qrCode"></div>
     </div>
 </template>
 <script>
@@ -11,19 +11,24 @@ export default {
     name: 'hello',
     data() {
         return {
-            userInfo: NormalHelper.userInfo()
+            userInfo: NormalHelper.userInfo(),
+            isManager: false
         }
     },
     mounted() {
-        this.$nextTick(function () {
-            require ('../../../lib/common/jquery.qrcode.min.js');
-            this.qrcode();
-        })
+        // this.isManager = this.userInfo.manager_type == 1 ? false : true;
+        this.isManager = true;
+        if(this.isManager){
+            this.$nextTick(function () {
+                require ('../lib/common/jquery.qrcode.min.js');
+                this.qrcode();
+            })
+        }
     },
     methods: {
         qrcode() {
             $("#qrCode").qrcode({
-                text: "http://www.baidu.com",
+                text: "https://weixin.zuban8.com/zuban_web/console/#/registered?region="+JSON.stringify([this.userInfo["region_code"],this.userInfo["region_name"]]),
                 width:200,
                 height:200
             })
@@ -36,5 +41,13 @@ export default {
     h1 {
         text-align: center;
     }
+}
+.container-qrCode {
+    width: 100%;
+    text-align: center;
+    position: fixed;
+    top: 200px;
+    bottom: 300px;
+    margin-left: -100px;
 }
 </style>
