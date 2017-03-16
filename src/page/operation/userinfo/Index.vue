025413600dc66a21b7951c76f07c5db8358c4c89
LG <171736699@qq.com>
2017-03-16 21:23:40 +0800

    用户管理

--- src/page/operation/userinfo/Index.vue
+++ src/page/operation/userinfo/Index.vue
@@ -1,50 +1,44 @@
 <template>
     <div>
         <div class="container-body">
-            <div class="header" v-if="managerType > 0">
-                <div class="search">
-                    <el-form :model="form" ref="form" label-width="120px">
-                        <el-form-item label="代理区域">
-                            <el-col :span="8">
-                                <el-form-item prop="region1">
-                                    <el-select class="select" v-model="form.region1" @change="selectRegion1List" placeholder="全国">
-                                        <el-option :label="item.name" :value="item.code" v-for="(item,index) in region1List"></el-option>
-                                    </el-select>
-                                </el-form-item>
-                            </el-col>
-                            <el-col :span="8">
-                                <el-form-item prop="region2">
-                                    <el-select class="select" v-model="form.region2" @change="selectRegion2List" placeholder="全国">
-                                        <el-option :label="item.name" :value="item.code" v-for="(item,index) in region2List"></el-option>
-                                    </el-select>
-                                </el-form-item>
-                            </el-col>
-                            <el-col :span="8">
-                                <el-form-item prop="region3">
-                                    <el-select class="select" v-model="form.region3" placeholder="全国">
-                                        <el-option :label="item.name" :value="item.code" v-for="(item,index) in region3List"></el-option>
-                                    </el-select>
-                                </el-form-item>
-                            </el-col>
+            <div class="search">
+                <el-form :model="form" ref="form" label-width="120px">
+                    <el-col :span="12">
+                        <el-form-item label="注册手机号" prop="phone">
+                            <el-input v-model.number="form.phone" placeholder="请输入注册手机号"></el-input>
                         </el-form-item>
-                        <el-form-item>
-                            <el-button type="primary" @click="submitForm('form')">提交</el-button>
-                            <el-button @click="resetForm('form')">重置</el-button>
+                    </el-col>
+                    <el-col :span="12">
+                        <el-form-item label="用户昵称" prop="name">
+                            <el-input v-model.number="form.name" placeholder="请输入用户昵称"></el-input>
                         </el-form-item>
-                    </el-form>
-                </div>
+                    </el-col>
+                    <el-form-item>
+                        <el-button type="primary" @click="submitForm('form')">提交</el-button>
+                        <el-button @click="resetForm('form')">重置</el-button>
+                    </el-form-item>
+                </el-form>
             </div>
             <div class="table">
-                <el-table :data="userStatisticsList" border style="width: 100%">
-                    <el-table-column prop="day_name" align="center" label="统计类型" min-width="90">
+                <el-table :data="userList" border style="width: 100%">
+                    <el-table-column prop="nick_name" align="center" label="用户昵称" min-width="90">
                     </el-table-column>
-                    <el-table-column prop="all_num" align="center" label="总注册用户数" min-width="120">
+                    <el-table-column prop="account" align="center" label="注册手机" min-width="120">
                     </el-table-column>
-                    <el-table-column prop="register_num" align="center" label="当日注册用户数" min-width="130">
+                    <el-table-column prop="register_time" align="center" label="注册时间" min-width="130">
                     </el-table-column>
-                    <el-table-column prop="login_num" align="center" label="当日活跃用户数" min-width="120">
+                    <el-table-column prop="status_name" align="center" label="账号状态" min-width="120">
+                    </el-table-column>
+                    <el-table-column fixed="right" align="center" label="操作" width="140">
+                        <template scope="scope">
+                            <el-button @click="deleteUser(scope.$index, userList)" size="small">
+                                {{scope.row.status==1?"禁用":"正常"}}
+                            </el-button>
+                        </template>
                     </el-table-column>
                 </el-table>
+                <el-pagination class="el-pagination" @current-change="handleCurrentChange" :page-size="10" layout="total, prev, pager, next" :total="total">
+                </el-pagination>
             </div>
         </div>
     </div>
@@ -56,91 +50,42 @@ export default {
         return {
             form: {
                 phone: '',
-                region1: '',
-                region2: '',
-                region3: ''
+                name: '',
+                status: 1
             },
-            region1List: [],
-            region2List: [],
-            region3List: [],
-            userStatisticsList: [],
+            userList: [],
             page: 1,
-            userInfo: NormalHelper.userInfo(),
-            managerType: 0
+            total: 0
         }
     },
     mounted() {
-
-        this.managerType = this.userInfo["manager_type"];
-        if(this.managerType <= 0){
-            this.form.region3 = this.userInfo["region_code"];
-        }else{
-            this.getRegionList();
-        }
-        this.getUserStatistics();
+        this.getUserList();
     },
     methods: {
-        getRegionList() {
-            var param = {
-                c: 'Zb',
-                m: 'Region',
-                a: 'getRegionList',
-                fixAll: 1
-            };
-            var p_obj = {
-                action: '',
-                param: param,
-                success: (response) => {
-                    this.region1List = response;
-                    this.region2List = response[0].children;
-                    this.region3List = response[0].children[0].children;
-                    this.form.region1 = response[0].code;
-                    this.form.region2 = response[0].children[0].code;
-                    this.form.region3 = response[0].children[0].children[0].code;
-                },
-                fail: (response) => {
-                    NormalHelper.alert(this, response, 'error');
-                }
-            };
-            AjaxHelper.GetRequest(p_obj);
-        },
-        selectRegion1List() {
-            for (var i = 0; i < this.region1List.length; i++) {
-                if (this.form.region1 == this.region1List[i].code) {
-                    this.region2List = this.region1List[i].children;
-                    this.region3List = this.region1List[i].children[0].children;
-                    this.form.region2 = this.region1List[i].children[0].code;
-                    this.form.region3 = this.region1List[i].children[0].children[0].code;
-                }
-            }
-        },
-        selectRegion2List() {
-            for (var i = 0; i < this.region2List.length; i++) {
-                if (this.form.region2 == this.region2List[i].code) {
-                    this.region3List = this.region2List[i].children;
-                    this.form.region3 = this.region2List[i].children[0].code;
-                }
-            }
-        },
-        getUserStatistics() {
+        getUserList() {
             var param = {
                 c: 'Admin',
-                m: 'Report',
-                a: 'userStatistics'
+                m: 'User',
+                a: 'getUserList',
+                status: this.form.status,
+                page: this.page,
+                row: 10
             };
-
-            if (this.form.region3 != "" && this.form.region3 != '1') {
-                param.region = this.form.region3;
+            if (this.form.phone != "") {
+                param.phone = this.form.phone;
+            }
+            if (this.form.name != "") {
+                param.name = this.form.name;
             }
             var p_obj = {
                 action: '',
                 param: param,
                 success: (response) => {
-
-                    this.userStatisticsList = [];
-                    for(var item in response){
-                        this.userStatisticsList.push(response[item]);
+                    for (var i = 0; i < response.list.length; i++) {
+                        response.list[i].status_name = response.list[i].status == 1 ? "正常" : "禁用";
                     }
+                    this.userList = response.list;
+                    this.total = parseInt(response.total);
                 },
                 fail: (response) => {
                     NormalHelper.alert(this, response, 'error');
@@ -148,28 +93,38 @@ export default {
             };
             AjaxHelper.GetRequest(p_obj);
         },
-        edit(index, rows) {
-
+        deleteUser(index, rows) {
+            var param = {
+                c: 'Admin',
+                m: 'User',
+                a: 'deleteUser',
+                userId: rows[index].user_id
+            };
+            var p_obj = {
+                action: '',
+                param: param,
+                success: (response) => {
+                    this.getUserList();
+                }
+            };
+            AjaxHelper.GetRequest(p_obj);
         },
         submitForm(formName) {
             this.$refs[formName].validate((valid) => {
                 if (valid) {
-                    if (this.form.region3 != "") {
-                        this.page = 1;
-                        this.getUserStatistics();
-                    }
+                    this.page = 1;
+                    this.getUserList();
                 } else {
                     return false;
                 }
             });
         },
         resetForm(formName) {
-            this.region2List = this.region1List[0].children;
-            this.region3List = this.region1List[0].children[0].children;
             this.$refs[formName].resetFields();
-            this.form.region1 = this.region1List[0].code;
-            this.form.region2 = this.region2List[0].code;
-            this.form.region3 = this.region3List[0].code;
+        },
+        handleCurrentChange(val) {
+            this.page = val;
+            this.getUserList();
         }
     },
     destroyed() {}
