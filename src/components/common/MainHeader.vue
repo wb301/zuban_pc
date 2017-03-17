b363dcb1fa9e815e854da27a99cca5a6b9e2b39a
沈冬旭 <18001637008@163.com>
2017-03-18 00:27:16 +0800

    注销

--- src/components/common/MainHeader.vue
+++ src/components/common/MainHeader.vue
@@ -2,6 +2,15 @@
     <header class="main-header">
         <div class="title">租伴网</div>
         <div class="right">
+            <el-dropdown @command="outLogin">
+                <span class="el-dropdown-link">
+                    {{userInfo.nick_name}}<i class="el-icon-caret-bottom el-icon--right"></i>
+                  </span>
+                <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item disabled>{{userInfo.account}}</el-dropdown-item>
+                    <el-dropdown-item command="outLogin">注销</el-dropdown-item>
+                </el-dropdown-menu>
+            </el-dropdown>
         </div>
     </header>
 </template>
@@ -10,13 +19,32 @@ export default {
     name: 'main-header',
     data() {
         return {
-            sellerList: [],
-            seller_name: ''
+            userInfo: ''
         }
     },
-    mounted: function() {},
+    mounted() {
+        this.userInfo = NormalHelper.userInfo();
+        SaveDataHelper.setLocalStorage("a","1");
+    },
     methods: {
-
+        outLogin() {
+            this.$confirm('您确定要退出登陆吗?', '提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+            }).then(() => {
+                SaveDataHelper.delCookie(GlobalModel.COOKIE_USER_INFO);
+                localStorage.clear();
+                this.$router.push({
+                    path: '/login'
+                });
+            }).catch(() => {
+                this.$message({
+                    type: 'info',
+                    message: '已取消'
+                });
+            });
+        }
     }
 }
 </script>
@@ -34,6 +62,19 @@ export default {
         min-height: 100%;
         line-height: 60px;
         display: inline-block;
+    }
+    .right {
+        position: absolute;
+        right: 21px;
+        top: 0;
+        min-height: 100%;
+        color: #ffffff;
+        line-height: 60px;
+        .el-dropdown {
+            cursor: pointer;
+            color: #FFFFFF;
+            font-size: 16px;
+        }
     }
 }
 </style>
