f511b34d980d936fd053ca32451072b2fd8f0542
LG <171736699@qq.com>
2017-03-18 01:24:44 +0800

    fix

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -31,12 +31,33 @@ export default {
     },
     methods: {
         qrcode() {
+            var text = this.utf16to8("https://weixin.zuban8.com/zuban_web/console/#/registered?region="+JSON.stringify(this.regionInfo));
             $("#qrCode").qrcode({
-                text: "https://weixin.zuban8.com/zuban_web/console/#/registered?region="+JSON.stringify(this.regionInfo),
+                text: text,
                 width:200,
-                height:200
-            })
-        }
+                height:200,
+                correctLevel:0
+            });
+        },
+        utf16to8(str) {
+           var out, i, len, c;
+           out = "";
+           len = str.length;
+           for(i = 0; i < len; i++) {
+               c = str.charCodeAt(i);
+               if ((c >= 0x0001) && (c <= 0x007F)) {
+                   out += str.charAt(i);
+               } else if (c > 0x07FF) {
+                   out += String.fromCharCode(0xE0 | ((c >> 12) & 0x0F));
+                   out += String.fromCharCode(0x80 | ((c >>  6) & 0x3F));
+                   out += String.fromCharCode(0x80 | ((c >>  0) & 0x3F));
+               } else {
+                   out += String.fromCharCode(0xC0 | ((c >>  6) & 0x1F));
+                   out += String.fromCharCode(0x80 | ((c >>  0) & 0x3F));
+               }
+           }
+           return out;
+       }
     },
 }
 </script>
